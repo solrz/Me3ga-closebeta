ffc112d957ceb1d493960ed832184729cc9bac59
solrz <kbpsolrz@gmail.com>
2021-02-23 02:19:06 +0800

    - [修正]將today.svelte中的公告區段改用可重用的components替代

--- src/pages/today.svelte
+++ src/pages/today.svelte
@@ -14,7 +14,8 @@
 
       BlockTitle.font-serif 近期公告
       Card(class="h-1/4").overflow-y-scroll
-        AnnouncementList(bind:this="{componentOfAnnouncementList}")
+        AnnouncementList(bind:this="{componentOfAnnouncementList}"
+          filter!="{a => !$newe3UserConfig.readAnnouncements.includes(a.id) && a.course.shortname.includes($newe3UserConfig.semester+'.')}")
 
 </template>
 <script>
@@ -22,13 +23,15 @@ import {Page, Navbar} from 'framework7-svelte'
 import {Row, Col} from 'framework7-svelte'
 import {Card, CardHeader, CardContent, Link} from 'framework7-svelte'
 import {BlockTitle, Block} from 'framework7-svelte'
+import {newe3Config, newe3Cache, newe3UserConfig} from '../js/store/e3Store.js';
 
 import OneDayCoursesGantt, {getCourses} from '../components/one-day-courses-gantt.svelte'
-import AnnouncementList from '../components/announcement-list.svelte'
+import AnnouncementList from '../components/announcements-view.svelte'
 import HomeworkGantt from '../components/homework-gantt.svelte'
 import TodaySummary from '../components/today-summary.svelte'
 
 let componentOfOneDayCoursesGantt, componentOfHomeworkGantt, componentOfAnnouncementList;
+$: semester = $newe3UserConfig.semester
 
 function refresh(done) {
   Promise.all([
