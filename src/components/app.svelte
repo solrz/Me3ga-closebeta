8e5cb012f86714f82b4d00ec80f5037686fa8c24
solrz <kbpsolrz@gmail.com>
2021-03-03 00:02:30 +0800

    - 清理不必要的匯入

--- src/components/app.svelte
+++ src/components/app.svelte
@@ -2,8 +2,6 @@
 <template lang="pug">
   LoginScreen.e3-login-page
     LoginScreenContent
-  PopUp
-  LoginScreenPage
   TabView
 
 </template>
@@ -11,8 +9,6 @@
 <script>
 import {onMount} from 'svelte';
 import TabView from './tab-view.svelte'
-import LoginScreenPage from './login-screen.svelte'
-import PopUp from './pop-up.svelte'
 import LoginScreenContent from '../components/login-screen.svelte'
 import {newe3Config, newe3Cache, newe3UserConfig} from '../js/store/e3Store.js';
 import {e3api} from '../js/api/e3Api.svelte'
@@ -20,38 +16,21 @@ newe3Config.useLocalStorage();
 newe3Cache.useLocalStorage();
 newe3UserConfig.useLocalStorage();
 
+
 import {
   f7,
   f7ready,
   App,
-  Panel,
-  Views,
-  View,
-  Popup,
-  Page,
-  Navbar,
-  Toolbar,
-  NavRight,
-  Link,
-  Block,
-  BlockTitle,
   LoginScreen,
-  LoginScreenTitle,
-  List,
-  ListItem,
-  ListInput,
-  ListButton,
-  BlockFooter
 } from 'framework7-svelte';
 
 
 import routes from '../js/routes';
 import store from '../js/store';
 
-
 // Framework7 Parameters
 let f7params = {
-  name: 'make-E3-great-again', // App name
+  name: 'NCTU-MEGA', // App name
   theme: 'auto', // Automatic theme detection
   store: store,
   routes: routes,
@@ -60,6 +39,7 @@ let f7params = {
   },
 };
 
+// import * as firebase from 'firebase/app'
 onMount(() => {
   f7ready(() => {
     if(!$newe3Config.token){
@@ -69,6 +49,7 @@ onMount(() => {
     }
   });
 })
+
 </script>
 <style global lang="postcss">
 
